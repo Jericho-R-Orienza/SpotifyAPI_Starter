6c4a31089c3adfcdf75c7dc7e4cdf9ced439223a
Jericho Orienza <jkr.orienza@gmail.com>
2024-03-08 21:15:06 -0500

    Initial Commit

---
+++ SpotifyAPI_Starter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpotifyAPI_Starter
+//
+//  Created by Jericho Orienza on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotifyAPI_Starter/SpotifyAPI_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyAPI_StarterApp.swift
+//  SpotifyAPI_Starter
+//
+//  Created by Jericho Orienza on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyAPI_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
